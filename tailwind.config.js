9327bbe3607e820910294897fadbd96247c83b6d
akmalamang <am.maulana997@gmail.com>
2022-04-23 09:16:35 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container :{
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#166534',
+        dark : '#0f172a',
+        secondary : '#64748b',
+      },
+      screen :{
+        '2xl' : '1410px',
+      },
+    },
+  },
+  plugins: [],
+}
